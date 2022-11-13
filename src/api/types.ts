39962bcf8463fec81acf64aede12a55e5d039101
Tyler Mueller <muelltyl@gmail.com>
2022-11-13 16:19:58 -0600

    rewrote collection

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,16 +1,36 @@
-export default interface Root {
-  users: User[];
-  posts: Post[];
+// export default interface Root {
+//   users: User[];
+//   posts: Post[];
+// }
+
+// export interface User {
+//   id: string;
+//   name: string;
+//   age: number;
+//   posts: Post[];
+// }
+
+// export interface Post {
+//   author: User;
+//   title: string;
+//   posted: number;
+//   content: string;
+//   views: number;
+// }
+
+import { Collection, FakeArray } from "src/api/mod.ts";
+
+export default interface ROOT {
+  users: FakeArray<User[]>;
+  posts: FakeArray<Post[]>;
 }
 
-interface User {
-  id: string;
+interface User extends Collection {
   name: string;
   age: number;
-  posts: Post[];
 }
 
-interface Post {
+interface Post extends Collection {
   author: User;
   title: string;
   posted: number;
