6bee7f83494cc23d74eabe985c2298fe8e3cbb01
Tyler Mueller <muelltyl@gmail.com>
2022-11-13 20:46:38 -0600

    second iteration

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,28 +1,46 @@
-// deno-lint-ignore-file no-empty-interface no-namespace
+import { Node, Collection } from 'src/api/mod.ts';
 
-import { Node, Node_Collection } from 'src/api/mod.ts';
-
-type node<T> = {
-  [P in keyof T]: Promise<T[P]>
-}
-
-type collection<T> = {
+type collect<T> = {
   [P in keyof T]: Array<T[P]>
 }
 
-export type structs = {
-  user: user;
-  post: post;
+export type node_collection = collect<node_types>;
+export type data_collection = collect<data_types>;
+
+export type node_types = {
+  user: node_user;
+  post: node_post;
 }
 
-interface user {
+export type data_types = {
+  user: data_user;
+  post: data_post;
+}
+
+// ---------------------------
+
+interface node_user {
   name: string;
   age: number;
-  posts: post[];
+  posts: Collection<'post'>;
 }
-interface post {
+
+interface node_post {
+  author: Node<'user'>;
   title: string;
-  author: user;
   content: string;
 }
 
+// ----------------------------
+
+interface data_user {
+  name: string;
+  age: number;
+  posts: data_post[];
+}
+
+interface data_post {
+  author: data_user;
+  title: string;
+  content: string;
+}
