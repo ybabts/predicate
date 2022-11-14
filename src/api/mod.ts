ca94fea215247800140133661180a5ef559cd8fa
Tyler Mueller <muelltyl@gmail.com>
2022-11-13 20:46:29 -0600

    second iteration

--- src/api/mod.ts
+++ src/api/mod.ts
@@ -1,34 +1,45 @@
 // deno-lint-ignore-file no-namespace
 
-export class Node<T extends keyof structs> {
-  constructor(type: T, data: structs[T]) {
+namespace Node {
+  export type identifier = string;
+  export type data = Record<string, unknown>;
+}
 
+import { struct as data_struct, struct_collection as data_struct_collection } from 'src/api/struct.ts';
+import { struct as node_struct, struct_collection as node_struct_collection} from 'src/api/nodes.ts';
+
+
+export class Node<T extends keyof node_struct> {
+  identifier: Node.identifier;
+  constructor(id: Node.identifier, type: T) {
+    this.identifier = id;
   }
-  filter() {
-
+  get data(): Promise<node_struct[T]> {
+    
   }
 }
 
-export class Node_Collection<T> {
-  constructor(arr: T[]) {
+export class Collection<T extends keyof node_struct_collection> {
+  constructor(type: T) {
 
   }
-  async all() {
+  async filter(predicate: (node: data_struct[T], index: number) => boolean): Promise<node_struct_collection[T]> {
 
   }
-  async filter(predicate: (value: T, index: number, array: never[]) => boolean) {
-    // do the shit
+  async all(): Promise<node_struct_collection[T]> {
+
+  }
+  pass(variables: Record<string,unknown>) {
+    return this;
   }
 }
 
-import { structs } from "src/api/types.ts";
 
+// Node.prototype.data is a promise that contains the
+// data of that node. If the data needs to be accessed
+// by the getter which does a fetch request to the server.
 
+const users = await new Collection('user').filter(v => v.age > 21);
+const posts = await Promise.all(users.map(v => v.posts.all()));
 
-export function collection<T extends keyof structs>(id: T): structs[T] {
-
-}
-
-const posts = collection('user')
-
-const node = new Node('post')
+console.log(posts.flat());
